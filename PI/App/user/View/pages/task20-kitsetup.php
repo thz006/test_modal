56c1662a399bb5be7781e064e2fa3da0dee0e2ae
Arthur <arthursantorogomes92@gmail.com>
2025-02-26 07:31:32 -0400

    rfrgdfgbdt

--- PI/App/user/View/pages/task20-kitsetup.php
+++ PI/App/user/View/pages/task20-kitsetup.php
@@ -615,41 +615,7 @@
             </div>
             <div class="right-arrow"> <img src="../../../../public/assets/img/right-arrow.png" alt="left-arrow"></div>
         </div>
-    </div>
-
-    <div class="overlay" id="overlay" style="display: none;"></div>
-
-       <!-- Modal (inicialmente oculto) -->
-    <div class="info-modal-container" id="infoModal" style="display: none;">
-        <div class="info-modal">
-            <h1 class="info-modal-title">CONFIGURAÇÕES</h1>
-            <p class="info-modal-placa-mae">
-                <img src="../../public/assets/img/modal1.png" alt="" class="icons-model">
-                1 x Placa Mae Gigabyte B550M Aorus Elite DDR4 Socket AM4 Chipset AMD B550
-            </p>
-            <p class="info-modal-placa-de-video">
-                <img src="../../public/assets/img/modal2.png" alt="" class="icons-model">
-                1 x Placa de Video PNY Quadro T400, 4GB, GDDR6, 64-bit, VCNT4004GB-PB
-            </p>
-            <p class="info-modal-armazenamento">
-                <img src="../../public/assets/img/modal3.png" alt="" class="icons-model">
-                1 x SSD Rover, 512GB, M.2 2280, PCIe NVMe 3.0, Leitura 2200 MB/s, Gravação 1700 MB/s, PCH-RVRP-512
-            </p>
-            <p class="info-modal-fonte">
-                <img src="../../public/assets/img/modal4.png" alt="" class="icons-model">
-                1 x Fonte Mancer Thunder 600W 80 Plus Bronze, MCR-THR600-BL01
-            </p>
-            <p class="info-modal-gabinete">
-                <img src="../../public/assets/img/modal5.png" alt="" class="icons-model">
-                1 x Gabinete Gamer Pouter 3 Lateral De Vidro Preto, PG-P03-BK
-            </p>
-            <p class="info-modal-alimentacao">
-                <img src="../../public/assets/img/modal6.png" alt="" class="icons-model">
-                1 x Cabo de Força 1.2m, 3 Pinos, Vermelho, PG-PWC-RD01
-            </p>
-        </div>
-    </div>
-    <script src="../../../../public/js/task20-modal.js"></script>
+    </div>    
 </body>
 </html>
 <?php include __DIR__.'/../../../../includes/footer.php'; ?>
